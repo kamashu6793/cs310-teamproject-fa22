4fed84b582d83af316c86c7631bc4ac607bb3d1e
Adam Parton <aparton1@stu.jsu.edu>
2022-10-17 11:34:16 -0500

    Added shift class

---
+++ src/edu/jsu/mcis/cs310/tas_fa22/Shift.java
@@ -0,0 +1,18 @@
+package edu.jsu.mcis.cs310.tas_fa22;
+
+/**
+ *
+ * @author Adam Parton
+ */
+
+import java.time.LocalTime;
+import java.util.HashMap;
+
+public class Shift {
+    
+    
+    
+    
+    
+    
+}
