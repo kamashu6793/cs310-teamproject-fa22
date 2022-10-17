98398fe6c037f2410f6705e16ae4ecad47a28b44
Will McAdams <wmcadams@stu.jsu.edu>
2022-10-17 13:19:20 -0500

    Added imports and Initialized variables

--- src/edu/jsu/mcis/cs310/tas_fa22/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/Punch.java
@@ -1,7 +1,21 @@
-
 package edu.jsu.mcis.cs310.tas_fa22;
+
+ import java.time.LocalDateTime;
+ import java.sql.*;
+ import java.time.DayOfWeek;
+ import java.time.LocalDate;
+ import java.time.LocalTime;
+ import java.time.format.DateTimeFormatter;
+ import java.util.HashMap;
+ import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class Punch {
     
+    private int terminalid, id, eventtypeid;
+    private PunchAdjustmentType punchtypeid;
+    private String badgeid, adjustmenttype;
+    private LocalDateTime timestamp, adjustedtimestamp;
+    private Badge badge;
+    private LocalTime adjustedtime;
     
 }
